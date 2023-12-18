9a3184ff5bf992685d852327bc0caf89eb24345f
Muthaiahprojects <muthubu01@gmail.com>
2023-12-18 20:00:00 +0530

    contact

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,16 @@
+let blue = document.querySelectorAll(".blue")
+let black = document.querySelectorAll(".black")
+
+function wCard(){
+  location.href = "./work.html"
+}
+
+for(let i=0;i<blue.length;i++){
+  blue[i].addEventListener("click",wCard)
+  black[i].addEventListener("click",wCard)
+}
+
+//carousel
 const myCarouselElement = document.querySelector('#myCarousel')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
@@ -5,3 +18,5 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   pause: false,
   touch: false
 })
+
+//work card
