f89e961d69279212d440057d2eabbc89152de29e
Muthaiahprojects <muthubu01@gmail.com>
2023-12-18 15:55:12 +0530

    about commit

--- assets/main.js
+++ assets/main.js
@@ -2,5 +2,6 @@ const myCarouselElement = document.querySelector('#myCarousel')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 1500,
+  pause: false,
   touch: false
 })
